59169dd8da0cd390cbe9b7c99f3f7b89a446465f
Alex Dixon <alex@conarrative.com>
2018-07-09 13:33:12 -0700

    Bump version

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
-(defproject fetch-fx "0.1.0-SNAPSHOT"
+(defproject fetch-fx "0.2.0"
   :description "re-frame fx handler with Javascript Fetch API."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src"]
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.238"]
                  [re-frame "0.10.5"]])
